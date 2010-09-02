2546a9c02f1e4f0725a4ce44d939d46c342079c3
Boryslav Larin <brabadu@gmail.com>
2010-09-02 22:06:48 +0300

    ticket:1 Model Person

--- testapp/models.py
+++ testapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Person (models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    bio = models.TextField()
+    telephone = models.CharField(max_length=13)
+    email = models.EmailField()
+    twitter = models.CharField(max_length=15)
+
+    def __unicode__(self):
+        return ("%s %s." % (self.last_name, self.first_name[0]) ).title()
+
