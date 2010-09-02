48b3981b328d0fc97ff9ba33d36a793b1d87d512
Boryslav Larin <brabadu@gmail.com>
2010-09-02 20:22:45 +0300

    ticket:1 Testing view for existance and context

--- testapp/tests.py
+++ testapp/tests.py
@@ -1,23 +1,23 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.test import Client
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+from django.core.urlresolvers import reverse
+
+class ContactsTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_view1(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Test that view exists and passes context
         """
-        self.failUnlessEqual(1 + 1, 2)
+        response = self.client.get(reverse('contacts'))
+        self.failUnlessEqual(response.status_code, 200)
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
+    def test_view2(self):
+        """
+        Test that passes context
+        """
+        response = self.client.get(reverse('contacts'))
+        self.failUnlessEqual(response.context[-1]['person'], "Larin B.")
 
