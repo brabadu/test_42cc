65061da1af0f5d9895a4bc035acba011c4718d33
Boryslav Larin <brabadu@gmail.com>
2010-09-02 22:07:47 +0300

    ticket:1 Views,urls,templates for person contacts

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from testapp.views import contacts
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -6,11 +7,9 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     # Example:
-    # (r'^test_42cc/', include('test_42cc.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^$',
+        view=contacts,
+        name='contacts'),
 
     # Uncomment the next line to enable the admin:
     # (r'^admin/', include(admin.site.urls)),
