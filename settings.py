0220c3d2a724958b7b44917197838421ccb636e3
Boryslav Larin <brabadu@gmail.com>
2010-09-02 16:45:38 +0300

    gitignore added, settings tweaks

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(os.path.dirname(__file__), 'db/db.sqlite'), # Or path to database file if using sqlite3.
+        'NAME': os.path.join(os.path.dirname(__file__), 'db.sqlite'), # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -93,6 +93,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+     'django.contrib.admin',
+     'test_42cc.testapp',
 )
 
