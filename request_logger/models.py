7070a1ad2f00db0ee9f9385ce04729682fd2e113
Boryslav Larin <brabadu@gmail.com>
2010-09-03 21:39:22 +0300

    ticket:3 Model and tests for requests logger

--- request_logger/models.py
+++ request_logger/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class RequestLogEntry (models.Model):
+    time = models.DateTimeField()
+    ip_address = models.CharField(max_length=15)
+    url = models.CharField(max_length=50)
+    request_method = models.CharField(max_length=10)
+    user_agent = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return "[RequestLogEntry: %s %s %s %s %s %s]" % (self.time.ctime(),
+                                            self.ip_address,
+                                            self.request_method,
+                                            self.url,
+                                            self.response,
+                                            self.user_agent,
+                                            )
+    def equals(self, log_entry):
+        return False
+
