b028292119a59b65a9c29075fc62104c1051cb8e
Boryslav Larin <brabadu@gmail.com>
2010-09-04 02:34:54 +0300

    ticket:4 Test for context processor

--- context_settings/tests.py
+++ context_settings/tests.py
@@ -1,23 +1,17 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.test import Client
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+from django.core.urlresolvers import reverse
+
+class SettingsContextTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def testcontext(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Test that passes context
         """
-        self.failUnlessEqual(1 + 1, 2)
-
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
+        response = self.client.get('/')
+        context = response.context['settings']
+        self.failUnlessEqual(context is not None, True)
 
