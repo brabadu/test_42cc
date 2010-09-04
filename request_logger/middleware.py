44ee8eefb80078ed2ecd7327424c6270a74d57e0
Boryslav Larin <brabadu@gmail.com>
2010-09-05 00:49:55 +0300

    ticket:3 PEP8 fixes

--- request_logger/middleware.py
+++ request_logger/middleware.py
@@ -2,6 +2,7 @@ import datetime
 
 from models import RequestLogEntry
 
+
 class RequestLogger():
     def process_request(self, request):
         time = datetime.datetime.now()
@@ -9,8 +10,11 @@ class RequestLogger():
         url = request.get_full_path()
         request_method = request.META['REQUEST_METHOD']
         user_agent = request.META.get('HTTP_USER_AGENT', 'Unknown')
-        r = RequestLogEntry(time=time, ip_address=ip_address, url=url, request_method=request_method, user_agent=user_agent)
+        r = RequestLogEntry(time=time,
+                            ip_address=ip_address,
+                            url=url,
+                            request_method=request_method,
+                            user_agent=user_agent)
         r.save()
 
         return None
-
