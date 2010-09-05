3227704fa1bcd20df47bdf61d71670e5a4a16b18
Boryslav Larin <brabadu@gmail.com>
2010-09-06 00:24:58 +0300

    ticket:5 Pass test 2.Save valid data post request

--- person_contacts/views.py
+++ person_contacts/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template.context import RequestContext
 from django.shortcuts import render_to_response
 
-from person_contacts.models import Person
+from person_contacts.models import Person, PersonForm
 
 
 def contacts(request):
@@ -13,4 +13,13 @@ def contacts(request):
 
 
 def contacts_edit(request):
-    return HttpResponse()
+    if request.method == 'POST':
+        p = Person.objects.get(pk=1)
+        form = PersonForm(request.POST, instance=p)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+        else:
+            return HttpResponse('form error')
+    else:
+        return HttpResponse('GET')
