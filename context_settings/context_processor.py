3a2737311e28c21d267b2dd97f7c670931fdcec9
Boryslav Larin <brabadu@gmail.com>
2010-09-06 20:51:45 +0300

    ticket:4 Fixed settings import
    Settings module import removed, changed to django.conf.settings object
    Context processor was simplified
    Removed outputting 'settings' to base.html

--- context_settings/context_processor.py
+++ context_settings/context_processor.py
@@ -1,9 +1,5 @@
-import settings
+from django.conf import settings
 
 
 def settings_processor(request):
-    setts_list = [i for i in dir(settings) if not i.startswith('__')]
-    setts_dict = dict(zip(setts_list,
-                          [getattr(settings, i) for i in setts_list]))
-
-    return {'settings': setts_dict}
+    return {'settings': settings}
