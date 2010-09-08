12748ff3fbdc07585c9cb0958c9242a72c40b3e7
Boryslav Larin <brabadu@gmail.com>
2010-09-08 22:18:08 +0300

    ticket:6 Widget form was added for date

--- person_contacts/forms.py
+++ person_contacts/forms.py
@@ -1,8 +1,17 @@
+import datetime
+
 from django.forms import ModelForm
+from django.forms.extras.widgets import SelectDateWidget
 
 from person_contacts.models import Person
 
 
 class PersonForm(ModelForm):
+    birth_date = SelectDateWidget()
+
     class Meta:
         model = Person
+        widgets = {
+            'birth_date': SelectDateWidget(years=range(1970,
+                                            datetime.datetime.now().year + 1)),
+        }
