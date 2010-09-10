75f6740f8f2ec7a940a597baea273f832e327ae9
Boryslav Larin <brabadu@gmail.com>
2010-09-10 18:31:32 +0300

    ticket:6 Widget was changed from standard to custom on the form

--- person_contacts/forms.py
+++ person_contacts/forms.py
@@ -4,15 +4,13 @@ from django.forms import ModelForm
 from django.forms.extras.widgets import SelectDateWidget
 
 from person_contacts.models import Person
+from person_contacts.widgets import JQueryDatePickerWidget
 
 
 class PersonForm(ModelForm):
-    birth_date = SelectDateWidget()
-
     class Meta:
         model = Person
         fields = [field.name for field in reversed(Person._meta.fields[1:])]
         widgets = {
-            'birth_date': SelectDateWidget(years=range(1970,
-                                            datetime.datetime.now().year + 1)),
+            'birth_date': JQueryDatePickerWidget(),
         }
