c15610e08660bb2733fe820316819dc1b20d74d3
Boryslav Larin <brabadu@gmail.com>
2010-09-15 21:39:49 +0300

    ticket:11 Loading image added

--- static_media/js/form.js
+++ static_media/js/form.js
@@ -5,10 +5,12 @@ $(document).ready(function() {
         url: '/ajax_contacts_edit/',
         beforeSubmit: function(formData, jqForm, options){
             $('input, textarea').attr('disabled', 'disabled');
+            $('img.loading').fadeIn('slow');
             return true;
         },
         success: function() { 
             $("input, textarea").removeAttr("disabled");
+            $('img.loading').fadeOut('slow');
         } 
     }); 
 });
