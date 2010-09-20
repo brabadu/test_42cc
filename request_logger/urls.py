4aae9d6007b0dbe493fd888327400571dd2cda97
Boryslav Larin <brabadu@gmail.com>
2010-09-20 19:16:53 +0300

    ticket:13 Sorting by priority implemented
    Sorting by priority is now available by adding 'sorting=asc' or
    'sorting=desc' in get-query string. If no 'sorting' option was provided,
    query is ordered by time of request

--- request_logger/urls.py
+++ request_logger/urls.py
@@ -3,18 +3,12 @@ from django.conf.urls.defaults import *
 from request_logger.models import RequestLogEntry
 
 
-urlpatterns = patterns('django.views.generic.list_detail',
+urlpatterns = patterns('',
     url(r'^$',
-        'object_list',
-        kwargs={
-            'queryset': RequestLogEntry.objects.order_by('time')[:10],
-            'template_object_name': 'entries',
-        },
+        'request_logger.views.request_list',
         name='request_log',
        ),
-)
 
-urlpatterns += patterns('',
     url(r'^priority/(\d+)/$',
         'request_logger.views.priority_list',
         name='priority_request_log',
--- request_logger/views.py
+++ request_logger/views.py
@@ -5,6 +5,22 @@ from django.views.generic.list_detail import object_list
 from request_logger.models import RequestLogEntry
 
 
+def request_list(request):
+    queryset = RequestLogEntry.objects.all()
+    sorting = request.GET.get('sorting', None)
+    if sorting == 'asc':
+        queryset = queryset.order_by('priority')[:10]
+    elif sorting == 'desc':
+        queryset = queryset.order_by('-priority')[:10]
+    else:
+        queryset = queryset.order_by('time')[:10]
+
+    return object_list(request, queryset=queryset,
+                                template_object_name='entries',
+                                extra_context={'sorting': sorting},
+                      )
+
+
 def priority_list(request, priority):
     queryset = RequestLogEntry.objects.filter(priority=priority)
     queryset = queryset.order_by('time')[:10]
