e229ac571b69ae9ecd313db921bf585f70e4e092
brabadu <brabadu@gmail.com>
2011-01-21 13:33:03 -0800

    Checking in-browser file editing

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 
+# another comment to check in-browser edition of file on github
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
